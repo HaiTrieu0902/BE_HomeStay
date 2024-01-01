fbdea480fd754f2362627a99d1db34db4d0ed880
Bùi Hải Triều <trieuql159@gmail.com>
2024-01-01 21:07:04 +0700

    merge

---
+++ src/controllers/common.controller.ts
@@ -0,0 +1,3 @@
+const CommonController = {};
+
+export default CommonController;
---
+++ src/routes/common.route.ts
@@ -0,0 +1,5 @@
+const express = require('express');
+const commonRoom = express.Router();
+// routeRoom.post('/create-room', AuthMiddleware.Authentication, AuthMiddleware.RoleManager, RoomController.CreateRoom);
+
+export default commonRoom;
